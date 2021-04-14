58a499cdf96903be80fcb72c6e135dc3dbef21f2
ppg02969 <bibliomania76ex@gmail.com>
2021-04-14 17:24:44 +0900

    トップページ修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,13 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
 
   def new
+    @tweets = Tweet.new
   end
 
   def create
+    @tweet = Tweet.create(tweet_params)
   end
 end
