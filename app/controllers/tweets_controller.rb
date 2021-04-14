0a65ffb0e39cdb5986bfca868474bcd3c922aded
ppg02969 <bibliomania76ex@gmail.com>
2021-04-14 20:37:01 +0900

    ツイート保存

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,10 +4,20 @@ class TweetsController < ApplicationController
   end
 
   def new
-    @tweets = Tweet.new
+    @tweet = Tweet.new
   end
 
   def create
     @tweet = Tweet.create(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render new_tweet_path
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :description, :voice).merge(user_id: current_user.id)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,10 @@
 class Tweet < ApplicationRecord
+  mount_uploader :voice, AudiofileUploader
+
+  belongs_to :user
+  has_many :comments
+
+  validates :voice, presence:true
+  validates :title, presence:true
+
 end
