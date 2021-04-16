1bd9d23892e2bb075d3fe515118eedee92b59790
ppg02969 <bibliomania76ex@gmail.com>
2021-04-17 00:33:03 +0900

    ユーザー情報編集ページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    #@tweets = @user.tweet
+    @tweets = @user.tweets
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+  
+  private
+  def user_params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   root to: 'tweets#index'
   post 'tweets/new' => 'tweets#new'
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
