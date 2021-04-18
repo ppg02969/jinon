90fd1f85cdb0fa9017edeb98b7648a42ce326147
ppg02969 <bibliomania76ex@gmail.com>
2021-04-18 14:04:34 +0900

    マイページ機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,16 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    if @user.update(user_params)
+      bypass_sign_in(@user)
+      redirect_to user_path
+    else
+      render :edit
+    end
   end
   
   private
   def user_params
+    params.require(:user).permit(:nickname, :email, :password, :age, :profile)
   end
 end
